a1fa4f2c52141cbf596c7636ab7d92789c316c07
Rina <rina.andria@gmail.com>
2018-07-11 19:04:32 +0200

    fix no image url

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -8,14 +8,23 @@ class Article extends React.Component<IArticle, {}> {
     return (
       <article className="Article">
         <div className="Article__image">
-          <Img src={[this.props.urlToImage, 'no-image.svg']} alt="the article's image" />
+          <Img
+            src={[this.props.urlToImage, `./no-image.jpg`]}
+            alt="the article's image"
+          />
         </div>
         <div className="Article__details">
           <div className="Article__details-text">
-            <a target="_blank" className="Article__details-title" href={this.props.url}>
+            <a
+              target="_blank"
+              className="Article__details-title"
+              href={this.props.url}
+            >
               <h2>{this.props.title}</h2>
             </a>
-            <p className="Article__details-description">{this.props.description}</p>
+            <p className="Article__details-description">
+              {this.props.description}
+            </p>
           </div>
           <div className="Article__details-metadata">
             <span className="Article__details-date">
